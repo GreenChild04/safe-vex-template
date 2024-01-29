0037a1b57b6bc0308f19db2a49b082a3bc5d5e95
GreenChild04 <greenchild04@protonmail.com>
2024-01-29 07:11:31 +0000

    Updated safe-vex-template to the latest version of safe-vex

--- src/bot.rs
+++ src/bot.rs
@@ -14,32 +14,24 @@ impl<'a> Bot<'a> for MyRobot {
     }
 
     #[inline]
-    fn autonomous(&'a mut self, _: &'a mut Context) {
+    fn autonomous(&'a mut self, _: &'a Context) {
         // Write your autonomous code here
     }
 
     #[inline]
-    fn opcontrol(&'a mut self, context: &'a mut Context) {
+    fn opcontrol(&'a mut self, context: &'a Context) {
         // Get state of controller joysticks
-        let left_stick: i8;
-        let right_stick: i8;
-        {
-            let mut controller = context.controller();
-            left_stick = controller.left_stick().clamp(8).y;
-            right_stick = controller.right_stick().clamp(8).y;
+        let left_stick = context.controller().left_stick().clamp(8).y;
+        let right_stick = context.controller().right_stick().clamp(8).y;
 
-            // Flush log if x button pressed
-            if controller.x() {
-                context.flush_logs();
-            }
+        // Flush log if x button pressed
+        if context.controller().x() {
+            context.flush_logs();
         }
 
         // Update the drivetrain / motors
-        self.drive.run(&context, left_stick, right_stick);
+        self.drive.run(context, left_stick, right_stick);
     }
 
-    #[inline]
-    fn disabled(&'a mut self, _: &'a mut Context) {
-        // This runs when the robot is in disabled mode
-    }
+    // note: there's no disabled runtime unlike in `vex-rt` (notify me if you want this feature)
 }
